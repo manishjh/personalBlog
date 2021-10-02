9f0e138d6cf7276ec7746d86286b97ff8c33a605
manishjh <manishjh@github.com>
2021-10-02 20:27:30 +0530

    new project mysite

---
+++ mysite/target/debug/deps/mysite-1302823ba2a64027.d
@@ -0,0 +1,5 @@
+/home/manish/projects/personal_site/mysite/target/debug/deps/mysite-1302823ba2a64027: src/main.rs
+
+/home/manish/projects/personal_site/mysite/target/debug/deps/mysite-1302823ba2a64027.d: src/main.rs
+
+src/main.rs:
---
+++ mysite/target/debug/mysite.d
@@ -0,0 +1,1 @@
+/home/manish/projects/personal_site/mysite/target/debug/mysite: /home/manish/projects/personal_site/mysite/src/main.rs
