f5a82feec283ed33d2fb7c5c8a962800daee856a
manishjh <manishjh@github.com>
2021-10-02 20:34:41 +0530

    removed target

--- mysite/target/debug/deps/mysite-1302823ba2a64027.d
+++
@@ -1,5 +0,0 @@
-/home/manish/projects/personal_site/mysite/target/debug/deps/mysite-1302823ba2a64027: src/main.rs
-
-/home/manish/projects/personal_site/mysite/target/debug/deps/mysite-1302823ba2a64027.d: src/main.rs
-
-src/main.rs:
--- mysite/target/debug/mysite.d
+++
@@ -1,1 +0,0 @@
-/home/manish/projects/personal_site/mysite/target/debug/mysite: /home/manish/projects/personal_site/mysite/src/main.rs
